9a7c2666a62745792490e01dd01dc1fc742e5328
Rishav Raj <52792420+R1i2@users.noreply.github.com>
2021-12-16 21:47:35 +0530

    Defining Routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const connectDB = require('./config/db');
 const app = express();
+connectDB();
 const PORT = process.env.PORT||5000;
 app.get("/",(req,res)=>{
     console.log('Hello from server.');
     res.send("NAMASTE WORLD");
 })
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
 
 app.listen(PORT,(err)=>{
     if(!err){
