169b6243a579812906c00d4b11b34ae776bed92b
Rishav Raj <52792420+R1i2@users.noreply.github.com>
2021-12-16 22:40:09 +0530

    Express validator Validation done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-//@route GET api/users
+const {check,validationResult} = require('express-validator/check');
+
+//@route POST api/users
 //@desc Test route
 //@access Public
-router.get("/",(req,res)=>{
+router.post("/",[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please enter a valid email').isEmail(),
+    check('password',"Please enter a valid password with 6 or more characters").isLength({min:6})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!(errors.isEmpty())){
+        return res.status(400).json({errors:errors.array()});
+    }
+    console.log(req.body);
     res.send("Users Routes");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 const PORT = process.env.PORT||5000;
+//Initialize middleware
+app.use(express.json({extended:false}));
 app.get("/",(req,res)=>{
     console.log('Hello from server.');
     res.send("NAMASTE WORLD");
